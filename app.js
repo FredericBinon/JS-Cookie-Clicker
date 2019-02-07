9ea248ef2252c0f772c9d20c37b1bad52ad41c32
Amalia Rodriguez <you@example.com>
2019-02-07 16:30:39 +0100

    add code

--- app.js
+++ app.js
@@ -1,45 +1,70 @@
-
-
-
 window.onload = () => {
-    // let score = 0;
-    // let button = document.getElementById("clic");
-    // let affichage = document.getElementById("affichage");
+    let multiObjet = {
+        prix: 50,
+        calprix(){
+            this.prix = this.prix * this.nombre;
+            return this.prix * this.nombre;
+        },
+        nombre: 1,
+        multiplicateur(){
+            
+            return ++this.nombre;
+        }
+    }    
 
-    // function clic() {
-    //     score++;
-    //     affichage.innerText = score;
-    // }
 
-    // button.addEventListener("click", function(e){
-    //     e.preventDefault();
-    //     clic();
-    // })
-    
+
+
     let score = 0;
     let multiplicateur = 1;
+
+
+
     let button = document.getElementById("clic");
     let affichage = document.getElementById("affichage");
     let multibutton = document.getElementById("multiplier");
+    let prixaffichage = document.getElementsByClassName("prix")[0];
+    let compteur = document.getElementsByClassName("compteur")[0];
 
-    function augmenterMultiplicateur () {
+
+
+
+  /*   function augmenterMultiplicateur () {
         multiplicateur++;
     }
-
+ */
     function click(multiplicateur) {
         for (var i= 1; i <= multiplicateur; i++) {
             score++;
         }
         affichage.innerText = score;
     }
+
+
+
+
 /* the button element allows to modifie the score*/
     button.addEventListener("click", function(e){
         e.preventDefault();
         click(multiplicateur);
     })
+
+
+
 /* the multiplicateur element allows to increment the score */
     multibutton.addEventListener("click", function(e){
         e.preventDefault();
-        augmenterMultiplicateur();
-    } )
+        //augmenterMultiplicateur();
+        multiObjet.calprix();
+        if (score >= multiObjet.prix) {
+            multiplicateur = multiObjet.multiplicateur();
+            score = score - multiObjet.prix;
+            prixaffichage.innerText = multiObjet.prix;
+            compteur.innerText = multiplicateur - 2;
+        }
+
+
+    })
+
+    
 }
