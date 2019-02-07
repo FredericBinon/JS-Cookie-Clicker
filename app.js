d0f883984f7634bb7046901b0cf1a828c01c03e9
Andrés Goldestein <andresgoldestein@gmail.com>
2019-02-07 14:03:21 +0100

    add_bg

--- app.js
+++ app.js
@@ -2,19 +2,7 @@
 
 
 window.onload = () => {
-    // let score = 0;
-    // let button = document.getElementById("clic");
-    // let affichage = document.getElementById("affichage");
-
-    // function clic() {
-    //     score++;
-    //     affichage.innerText = score;
-    // }
-
-    // button.addEventListener("click", function(e){
-    //     e.preventDefault();
-    //     clic();
-    // })
+   
     
     let score = 0;
     let multiplicateur = 1;
