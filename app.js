fdf5c81816c69bd9ae850c1c9d718c5380234caf
Andrés Goldestein <andresgoldestein@gmail.com>
2019-02-07 11:35:29 +0100

    add second function

--- app.js
+++ app.js
@@ -2,19 +2,44 @@
 
 
 window.onload = () => {
+    // let score = 0;
+    // let button = document.getElementById("clic");
+    // let affichage = document.getElementById("affichage");
+
+    // function clic() {
+    //     score++;
+    //     affichage.innerText = score;
+    // }
+
+    // button.addEventListener("click", function(e){
+    //     e.preventDefault();
+    //     clic();
+    // })
+    
     let score = 0;
+    let multiplicateur = 1;
     let button = document.getElementById("clic");
     let affichage = document.getElementById("affichage");
+    let multibutton = document.getElementById("multiplier");
 
-    function clic() {
-        score++;
-        affichage.innerText = score;
+    function augmenterMultiplicateur () {
+        multiplicateur++;
     }
 
+    function click(multiplicateur) {
+        for (var i= 1; i <= multiplicateur; i++) {
+            score++;
+        }
+        affichage.innerText = score;
+    }
+/* the button element allows to modifie the score*/
     button.addEventListener("click", function(e){
         e.preventDefault();
-        clic();
+        click(multiplicateur);
     })
-    
-
+/* the multiplicateur element allows to increment the score */
+    multibutton.addEventListener("click", function(e){
+        e.preventDefault();
+        augmenterMultiplicateur();
+    } )
 }
