621f85b1dbba3423f91c3864049634b8802fdcca
Mathilde <baquet.mathilde@gmail.com>
2019-02-08 11:47:27 +0100

    add autoclick 08/02

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-
-
-
 window.onload = () => {
     let multiObjet = {
         prix: 50,
@@ -10,23 +7,17 @@ window.onload = () => {
         },
         nombre: 1,
         multiplicateur(){
-            
             return ++this.nombre;
         }
     } 
+
     let score = 0;
     let multiplicateur = 1;
-
-
-
     let button = document.getElementById("clic");
     let affichage = document.getElementById("affichage");
     let multibutton = document.getElementById("multiplier");
     let prixaffichage = document.getElementsByClassName("prix")[0];
     let compteur = document.getElementsByClassName("compteur")[0];
-
-
-
 
   /*   function augmenterMultiplicateur () {
         multiplicateur++;
@@ -39,16 +30,11 @@ window.onload = () => {
         affichage.innerText = score;
     }
 
-
-
-
 /* the button element allows to modifie the score*/
     button.addEventListener("click", function(e){
         e.preventDefault();
         click(multiplicateur);
     })
-
-
 
 /* the multiplicateur element allows to increment the score */
     multibutton.addEventListener("click", function(e){
@@ -61,9 +47,5 @@ window.onload = () => {
             prixaffichage.innerText = multiObjet.prix;
             compteur.innerText = multiplicateur - 2;
         }
-
-
-    })
-
-    
+    })   
 }
