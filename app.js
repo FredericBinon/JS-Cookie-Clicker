2e3724081cfccdcb704e4834419d2e3bcf121fe2
Andrés Goldestein <andresgoldestein@gmail.com>
2019-02-11 12:18:02 +0100

    add

--- app.js
+++ app.js
@@ -1,26 +1,19 @@
-window.onload = () => {
 
+
+
+window.onload = () => {
     let multiObjet = {
         prix: 50,
-        nombre: 1,
-        multiplicateur: 1,
-        augmenterMultiplicateur(){
-            ++this.multiplicateur;
-            ++this.nombre;
-        },
         calprix(){
-                this.prix = this.prix * 2;
+            this.prix = this.prix * this.nombre;
+            return this.prix * this.nombre;
+        },
+        nombre: 1,
+        multiplicateur(){
+            
+            return ++this.nombre;
         }
-
-    }
-
-    let bonusObjet = {
-        prix: 5000,
-        tempsFin : 0,
-        tempsDebut : 0
-
-    }
-
+    } 
     let score = 0;
     let multiplicateur = 1;
     let button = document.getElementById("clic");
@@ -28,10 +21,14 @@ window.onload = () => {
     let multibutton = document.getElementById("multiplier");
     let prixaffichage = document.getElementsByClassName("prix")[0];
     let compteur = document.getElementsByClassName("compteur")[0];
-    let bouttonBonus = document.getElementById ("bonus");
 
 
 
+
+  /*   function augmenterMultiplicateur () {
+        multiplicateur++;
+    }
+ */
     function click(multiplicateur) {
         for (var i= 1; i <= multiplicateur; i++) {
             score++;
@@ -40,47 +37,7 @@ window.onload = () => {
     }
 
 
-    function game() {
-        let multiplicator = multiObjet;
 
-        if (score >= multiplicator.prix) {
-            score = score - multiplicator.prix;
-            multiplicator.calprix();
-            multiplicator.augmenterMultiplicateur();
-
-            multiplicateur = multiplicator.multiplicateur;
-
-            prixaffichage.innerText = multiplicator.prix;
-            compteur.innerText = "X" + multiplicator.nombre;
-            affichage.innerText = score;
-        } else {
-            document.getElementById("msg").style.display = "block";
-            setTimeout(function () {
-                document.getElementById("msg").style.display = "none";
-           }, 3000 ); 
-           
-        }
-    }
-    function bonus () {
-        let bonusObj = bonusObjet;
-        if (score < bonusObj.prix) {
-            console.log("message erreur")
-            //message erreur s'affiche
-        }
-        else if (score >= bonusObj.prix) {
-            let i;
-            let dat = new Date();
-            bonusObj.tempsDebut = dat.getSeconds();
-            bonusObj.tempsFin = bonusObj.tempsDebut + 30;
-            do{
-                if(bonusObj.tempsFin > new Date().getSeconds()) {
-                    return 1;
-                }
-                i++;
-            } while(i<1000)
-        }
-
-    }
 
 /* the button element allows to modifie the score*/
     button.addEventListener("click", function(e){
@@ -88,20 +45,22 @@ window.onload = () => {
         click(multiplicateur);
     })
 
+
+
 /* the multiplicateur element allows to increment the score */
     multibutton.addEventListener("click", function(e){
         e.preventDefault();
         //augmenterMultiplicateur();
+        multiObjet.calprix();
+        if (score >= multiObjet.prix) {
+            multiplicateur = multiObjet.multiplicateur();
+            score = score - multiObjet.prix;
+            prixaffichage.innerText = multiObjet.prix;
+            compteur.innerText = multiplicateur - 2;
+        }
 
-       game();
 
     })
 
-    bouttonBonus.addEventListener("click",function (e){
-        e.preventDefault ();
-
-        
-    })
-     
+    
 }
-
