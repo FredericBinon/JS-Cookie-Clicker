64ae1be336eb3ae3f6cb5f50538f45b226ba89a3
FredericBinon <fredericbinn@gmail.com>
2019-02-12 11:40:41 +0100

    oui

--- app.js
+++ app.js
@@ -25,11 +25,15 @@ window.onload = () => {
     let score = 0;
     let multiplicateur = 1;
     let boutonactive=false;
+    let besoin = 1;
+    let autoclicUtilises = 0;
+    let autoclicRestants = 3;
     let button = document.getElementById("clic");
     let affichage = document.getElementById("affichage");
     let multibutton = document.getElementById("multiplier");
     let autobutton = document.getElementsByName("autoclicker")[0];
     let prixaffichage = document.getElementsByClassName("prix")[0];
+    let prixAutoclicker= document.getElementsByClassName("prix")[1];
     let compteur = document.getElementsByClassName("compteur")[0];
     let compteautoclicker= document.getElementsByClassName("compteur")[1];
     let bouttonBonus = document.getElementById("bonus");
@@ -37,9 +41,7 @@ window.onload = () => {
 
     function click(multiplicateur) {
         let tempsFin = new Date().getSeconds();
-        console.log(multiplicateur);
         let bonObj = bonusObjet ;
-        console.log("djed: " + tempsFin + "fggg" +  bonObj.tempsDebut);
         if ( bonObj.tempsActif == 1 && tempsFin < bonObj.tempsDebut){
             multiplicateur = multiplicateur * 2 ;
             console.log("djed2: " + tempsFin);
@@ -61,6 +63,7 @@ window.onload = () => {
 
 
     function testautoclic(oldAutoclicRestants, oldBesoin, oldAutoclicUtilises){
+      console.log(besoin, autoclicRestants, autoclicUtilises)
       if (autoclicRestants<1) {
         document.getElementsByName("autoclicker")[0].className = "activated";
         alert("Bin non gros malin, t'as d'ja tout pris");
@@ -69,7 +72,7 @@ window.onload = () => {
         if (score<besoin){
             alert("Wesh ma gueule, il te faut plus de cookies pour ça");
         }
-        if (score >= oldBesoin) {
+        if (score >= besoin) {
           autoclicUtilises = oldAutoclicUtilises + 1;
           autoclicRestants = oldAutoclicRestants -1;
           score=score-oldBesoin;
@@ -84,9 +87,6 @@ window.onload = () => {
         if (autoclicRestants==0){
             document.getElementsByName("autoclicker")[0].className = "activated";
         }
-      }
-      else {
-              alert("erreur: Bin non gros malin, tu l'as deja activé");
       }
     }
 
