f28c136c23efce74cf3427ac1ae5fa3cf60d6212
Mathilde <baquet.mathilde@gmail.com>
2019-02-13 09:12:33 +0100

    lkjj

--- countdown.js
+++ countdown.js
@@ -1,6 +1,1 @@
-var decompte = function(i){
-    document.getElementById("decompte")innerHTML=i+"s";
-}
-var affichage=function(){
-    document.getElementById("decompte").innerHTML="fin du compte à rebour";
-}
+
